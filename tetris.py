a1386f530b53592e39b12344d1cb79d0d5692db4
datamine <datamine@users.noreply.github.com>
2014-11-27 00:35:54 -0600

    this-block-is-falling!

--- tetris.py
+++ tetris.py
@@ -1,10 +1,12 @@
 # John Loeber | Nov 26 | Python 2.7.8 | x86_64 Debian Linux | www.johnloeber.com
 from sys import exit
 from makecanvas import gridline, blocksize
-from PIL import ImageColor
+from ImageColor import getrgb
 from random import choice
 import pygame
 import time
+
+blockimages = {}
 
 class Block(object):
     def __init__(self,color,x,y):
@@ -12,28 +14,30 @@ class Block(object):
         self.x = x
         self.y = y
     def down(self):
-        self.y -= 1
+        self.y += 1
     def getposn(self):
-        return ((blocksize*self.x)+self.x,(blocksize*self.y)+self.y)
+        z = ((blocksize*self.x)+self.x+6,(blocksize*self.y)+self.y+6)
+        print z
+        return z
     def getimg(self):
-        return 
-"""
-class Shape(object):
-    def __init__(self,b1,b2,b3,b4):
-        self.b1 = b1
-        self.b2 = b2
-        self.b3 = b3
-        self.b4 = b4
-    def rotate(self):
-        
-        
-        
+        global blockimages
+        return blockimages[self.color]
+
+def makeblockimages():
+    """
+    Takes the size and colors of the blocks to make images of them.
+    """
+    global blockimages    
     # Colors of the Tetrominoes
     colors = ["#FFE922","#3CFF2D","#2F3AFF","#990084","#CC1100","#FF7E00","#065C00"]
-   
-"""  
+    for c in colors:
+        newblock = pygame.Surface((blocksize,blocksize))
+        newblock.fill(getrgb(c))
+        blockimages[c] = newblock
+    return
 
 def main():
+    makeblockimages()
     pygame.init()
     size = (341,700)
 
@@ -42,10 +46,10 @@ def main():
     blocks = [Block("#FFE922",4,0)]    
 
     background = pygame.image.load("Grid.PNG")
-    backgroundcolor = ImageColor.getrgb(gridline)
+    backgroundcolor = getrgb(gridline)
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT: exit()
         
         screen.fill(backgroundcolor)
         # unsure if this use of .blit() is the most efficient I could do
@@ -60,7 +64,7 @@ def main():
             block.down()
         for block in blocks:
             screen.blit(block.getimg(), block.getposn())
-        time.sleep(0.1)
+        time.sleep(1)
         pygame.display.flip()
 
 # let's say the user is allowed three 'moves' per downward step
