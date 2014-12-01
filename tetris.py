69e2f61af3a56d68dfc452caa79deb3855d6a2ad
datamine <datamine@users.noreply.github.com>
2014-11-30 21:29:25 -0600

    made-shapes

--- tetris.py
+++ tetris.py
@@ -1,4 +1,5 @@
 # John Loeber | 26-NOV-2014 | Python 2.7.8 | x86_64 Debian Linux | www.johnloeber.com
+
 from sys import exit
 from makecanvas import gridline, blocksize
 from ImageColor import getrgb
@@ -21,22 +22,13 @@ blockimages = {}
 # Colors of the Tetrominoes
 colors = ["#FFE922","#3CFF2D","#2F3AFF","#990084","#CC1100","#FF7E00","#065C00"]
 # these define the different shapes. Every shape is on a new line.
-shapes = [[Block(colors[0],4,i) for i in range(3)],
-          [Block(colors[1],i,0) for i in [3,4,5]].append(Block(colors[1],4,1)),
+shapes = [[Block(colors[0],4,i) for i in range(4)],
+          [Block(colors[1],i,0) for i in [3,4,5]] + [Block(colors[1],4,1)],
           [Block(colors[2],i,j) for i in [4,5] for j in [0,1]],
-          [Block(colors[3],4,i) for i in range(2)].append(Block(colors[2],5,2)),
-          [Block(colors[4],5,i) for i in range(2)].append(Block(colors[2],4,2)),
+          [Block(colors[3],4,i) for i in range(3)] + [Block(colors[2],5,2)],
+          [Block(colors[4],5,i) for i in range(3)] + [Block(colors[2],4,2)],
           [Block(colors[5],4,0),Block(colors[5],4,1),Block(colors[5],3,1),Block(colors[5],5,0)],
-          [Block(colors[6],5,0),Block(colors[6],5,1),Block(colors[6],3,0),Block(colors[6],6,1)]]
-
-class Shape(object):
-    def __init__(self,color,b1,b2,b3,b4):
-        self.color = color
-        # the coordinates of b1 are the 'center' of the shape
-        self.b1 = b1
-        self.b2 = b2
-        self.b3 = b3
-        self.b4 = b4
+          [Block(colors[6],5,0),Block(colors[6],5,1),Block(colors[6],4,0),Block(colors[6],6,1)]]
 
 def newtetromino():
     """
@@ -64,7 +56,6 @@ def shapemove(blocks,board,x,y):
     # make sure the given square is available
     available = True
     for block in blocks:
-        # try/except to ensure the blocks stay within the bounds of the board
         # assuming the board is of size 10 x 20
         if not (0 <= block.x+x < 10 and 0 <= block.y+y < 20):
             available = False
@@ -88,9 +79,7 @@ def main():
 
     screen = pygame.display.set_mode(size)
 
-    blocks = []
-    
-    blocks.append(randomshape())    
+    blocks = newtetromino()
     
     # allows for [x][y] indexing, but is actually a list of columns. A
     # bit unintuitive.
