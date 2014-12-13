55b51704a12fb3140064aefeda6d1741f52634dd
datamine <datamine@users.noreply.github.com>
2014-12-12 19:55:40 -0600

    updated-annotation

--- all/gameproperties.py
+++ all/gameproperties.py
@@ -5,6 +5,6 @@ blocksize = 32
 background = "#9B9B9B"
 gridline = "#545452"
 
-# Colors of the Tetriminoes, white for the flashing effect
+# Colors of the Tetriminoes, last entry (white) for the flashing effect
 colors = ["#FFE922","#3CFF2D","#2F3AFF","#990084","#CC1100","#FF7E00",
           "#065C00","#FFFFFF"]
